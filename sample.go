ca2ce051a0e474a39a07197ff5af6a70961eeb4b
sasaendou <sasa.ya823@gmail.com>
2023-06-20 22:43:13 +0900

    second commit

--- sample.go
+++ sample.go
@@ -1,10 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
 
 func main() {
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(add(42, 13))
 }
