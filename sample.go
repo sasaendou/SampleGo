e021d9ce03b7926a583a93f3cb86252df7233eae
sasaendou <sasa.ya823@gmail.com>
2023-07-01 19:45:40 +0900

    18th commit

--- sample.go
+++ sample.go
@@ -8,6 +8,8 @@ import (
 func pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
 	}
 	return lim
 }
@@ -15,6 +17,6 @@ func pow(x, n, lim float64) float64 {
 func main() {
 	fmt.Println(
 		pow(3, 2, 10),
-		pow(3, 2, 20),
+		pow(3, 3, 20),
 	)
 }
